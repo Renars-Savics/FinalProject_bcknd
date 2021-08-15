4a930fae5fbf66bb24e3252b74057b6a1d0dcfb2
Renars-Savics <renars.savics@protonmail.com>
2021-08-15 13:47:05 +0300

    Tests 3 Commit

---
+++ src/main/resources/testdb/migration/V1_0_2__insert_card.sql
@@ -0,0 +1,4 @@
+INSERT INTO card (card_holder, card_number, exp_date, account_id, status ) VALUES
+  ('Test Usr1', 'test001',current_date + 365, 1, 1),
+  ('Test Usr2', 'test002',current_date + 244, null, 1);
+
